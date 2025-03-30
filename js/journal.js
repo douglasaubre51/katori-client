da6f3223eae7714a9b97e773b8b9978fb0ae64f2
Allen Bose <boseallen192@gmail.com>
2025-03-30 13:58:49 +0530

    created fetch api for journal table

--- js/journal.js
+++ js/journal.js
@@ -1,1 +1,15 @@
 console.log('js is working!')
+
+fetch("http://localhost:5014/api/journal/getJournals")
+.then(response=>response.json())
+.then(data=>{
+    data.forEach(function(d){
+	console.log(d.id)
+	console.log(d.date)
+	console.log(d.particular1)
+	console.log(d.particular2)
+	console.log(d.comment)
+	console.log(d.debit)
+	console.log(d.credit)
+    })
+})
